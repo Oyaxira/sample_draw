24914041ad34b74d9828a2e97cfa83e79280bdb6
oyaxira <oyaxira@vip.qq.com>
2019-06-19 10:56:43 +0800

    add react

---
+++ app/javascript/packs/components/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './main.scss'
+
+export default class Main extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  render(){
+    return <div className="container">
+      哈哈哈
+    </div>
+  }
+}
